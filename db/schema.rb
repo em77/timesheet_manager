3eb4eb54fe41d9a235e80f048ddf0e645f667870
Eric M <ericmarchese77@gmail.com>
2016-12-22 13:17:57 -0500

    Add title column to companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222174951) do
+ActiveRecord::Schema.define(version: 20161222181657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20161222174951) do
     t.integer  "pay_freq"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "title"
   end
 
   create_table "employee_profiles", force: :cascade do |t|
