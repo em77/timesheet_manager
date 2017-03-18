a169aae0ef0242174a5e623986189560cc24e54c
Eric M <ericmarchese77@gmail.com>
2017-03-18 13:16:26 -0400

    Add 'Not Clocked Out' to Approved column of timesheets_table if timesheet is not clocked out. Show profileable_type on _user with profile_view_name method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def profile_view_name(profileable_type)
+    case profileable_type
+    when "AdminProfile"
+      return "Administrator"
+    when "EmployeeProfile"
+      return "Employee"
+    end
+  end
 end
