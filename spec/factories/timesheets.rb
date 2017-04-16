4c02b0c172f67f9d8233dbaa3a4e3a73a4f0a08b
Eric M <ericmarchese77@gmail.com>
2017-04-15 22:38:42 -0400

    Add TimesheetsHelper spec

--- spec/factories/timesheets.rb
+++ spec/factories/timesheets.rb
@@ -4,5 +4,19 @@ FactoryGirl.define do
       pay_period_id 1
       clock_in Time.new(2017, 4, 13, 14, 03, 0, "-04:00")
     end
+
+    trait :approved do
+      id 1
+      approved_status :approved
+      clock_in Time.new(2017, 4, 13, 14, 03, 0, "-04:00")
+      clock_out Time.new(2017, 4, 13, 20, 03, 0, "-04:00")
+    end
+
+    trait :unapproved do
+      id 2
+      approved_status :unapproved
+      clock_in Time.new(2017, 4, 15, 10, 03, 0, "-04:00")
+      clock_out Time.new(2017, 4, 15, 18, 03, 0, "-04:00")
+    end
   end
 end
--- spec/helpers/timesheets_helper_spec.rb
+++ spec/helpers/timesheets_helper_spec.rb
@@ -1,15 +1,34 @@
-require 'rails_helper'
+require "rails_helper"
 
-# Specs in this file have access to a helper object that includes
-# the TimesheetsHelper. For example:
-#
-# describe TimesheetsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe TimesheetsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#status_id_maker" do
+    it "should return proper status string" do
+      expect(helper.status_id_maker(5)).to eq "status-5"
+    end
+  end
+
+  describe "#status_css_maker" do
+    context "timesheet is approved" do
+      it "should return approved css class" do
+        timesheet = create(:timesheet, :approved)
+        expect(helper.status_css_maker(timesheet)).to eq "glyphicon" +
+          " glyphicon-ok status_approved"
+      end
+    end
+
+    context "timesheet is unapproved" do
+      it "should return unapproved css class" do
+        timesheet = create(:timesheet, :unapproved)
+        expect(helper.status_css_maker(timesheet)).to eq "status_unapproved"
+      end
+    end
+  end
+
+  describe "#timesheets_total_hours" do
+    it "should return total hours of timesheets collection" do
+      create(:timesheet, :approved)
+      create(:timesheet, :unapproved)
+      expect(helper.timesheets_total_hours(Timesheet.all)).to eq 14.0
+    end
+  end
 end
