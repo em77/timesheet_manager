09c05a1b2249a73e26a4e84c55ed92401671082a
Eric M <ericmarchese77@gmail.com>
2017-04-24 16:41:57 -0400

    Move upcoming pay date html into upcoming_pay_date_header helper method. Display 'No upcoming pay date' when there is no upcoming pay date for a job. Put _new_timesheet_link_form into panel.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,24 @@ module PagesHelper
   def latest_pay_period(job)
     job.pay_periods.where( pay_date: job.pay_periods.maximum(:pay_date) ).first
   end
+
+  def upcoming_pay_date_header(company_title, job_title, there_is_pay_date,
+    pay_date = nil)
+    if there_is_pay_date
+      pay_date_formatted = pay_date.strftime("%A, %b %d, %Y")
+    else
+      pay_date_formatted = "No upcoming pay date"
+    end
+    %Q(
+      <h3>
+        #{company_title}
+        <small>
+          #{job_title}
+        </small>
+        <span class="pay-date-text">
+          #{pay_date_formatted}
+        </span>
+      </h3>
+    ).html_safe
+  end
 end
