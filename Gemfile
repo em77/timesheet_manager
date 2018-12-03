a3f091cb28db682d781e5dc772671dd59b1af1a3
Eric M <ericmarchese77@gmail.com>
2018-12-02 23:46:43 -0500

    Update ruby and rails

--- Gemfile
+++ Gemfile
@@ -5,10 +5,10 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-ruby '2.4.4'
+ruby '2.4.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.5'
+gem 'rails', '~> 5.1.6.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
 # Use Puma as the app server
@@ -56,9 +56,6 @@ gem 'will_paginate', '~> 3.1.0'
 
 # For next/previous record
 gem 'order_query'
-
-# Backup database to S3 bucket
-gem 'pgbackups-archive'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/schema.rb
+++ db/schema.rb
@@ -15,101 +15,101 @@ ActiveRecord::Schema.define(version: 20170402200820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admin_profiles", force: :cascade do |t|
+  create_table "admin_profiles", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "companies", force: :cascade do |t|
-    t.integer  "admin_profile_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "title"
-    t.integer  "active_status"
+  create_table "companies", id: :serial, force: :cascade do |t|
+    t.integer "admin_profile_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.integer "active_status"
   end
 
   create_table "companies_employee_profiles", id: false, force: :cascade do |t|
     t.integer "company_id"
     t.integer "employee_profile_id"
-    t.index ["company_id"], name: "index_companies_employee_profiles_on_company_id", using: :btree
-    t.index ["employee_profile_id"], name: "index_companies_employee_profiles_on_employee_profile_id", using: :btree
+    t.index ["company_id"], name: "index_companies_employee_profiles_on_company_id"
+    t.index ["employee_profile_id"], name: "index_companies_employee_profiles_on_employee_profile_id"
   end
 
-  create_table "employee_profiles", force: :cascade do |t|
+  create_table "employee_profiles", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "jobs", force: :cascade do |t|
-    t.integer  "pay_cents"
-    t.integer  "pay_type"
-    t.integer  "employee_profile_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "title"
-    t.integer  "pay_freq"
-    t.integer  "company_id"
-    t.integer  "active_status"
-    t.index ["company_id"], name: "index_jobs_on_company_id", using: :btree
-    t.index ["employee_profile_id"], name: "index_jobs_on_employee_profile_id", using: :btree
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "read_status"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.string   "subject"
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
-    t.index ["recipient_id"], name: "index_messages_on_recipient_id", using: :btree
-    t.index ["sender_id"], name: "index_messages_on_sender_id", using: :btree
-  end
-
-  create_table "pay_periods", force: :cascade do |t|
-    t.date     "start_date"
-    t.date     "end_date"
-    t.date     "pay_date"
+  create_table "jobs", id: :serial, force: :cascade do |t|
+    t.integer "pay_cents"
+    t.integer "pay_type"
+    t.integer "employee_profile_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "job_id"
-    t.text     "notes"
-    t.index ["job_id"], name: "index_pay_periods_on_job_id", using: :btree
+    t.string "title"
+    t.integer "pay_freq"
+    t.integer "company_id"
+    t.integer "active_status"
+    t.index ["company_id"], name: "index_jobs_on_company_id"
+    t.index ["employee_profile_id"], name: "index_jobs_on_employee_profile_id"
   end
 
-  create_table "timesheets", force: :cascade do |t|
+  create_table "messages", id: :serial, force: :cascade do |t|
+    t.text "content"
+    t.integer "read_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "subject"
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.index ["recipient_id"], name: "index_messages_on_recipient_id"
+    t.index ["sender_id"], name: "index_messages_on_sender_id"
+  end
+
+  create_table "pay_periods", id: :serial, force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.date "pay_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "job_id"
+    t.text "notes"
+    t.index ["job_id"], name: "index_pay_periods_on_job_id"
+  end
+
+  create_table "timesheets", id: :serial, force: :cascade do |t|
     t.datetime "clock_in"
     t.datetime "clock_out"
-    t.integer  "approved_status"
-    t.integer  "approving_admin_profile_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.integer  "pay_period_id"
-    t.index ["pay_period_id"], name: "index_timesheets_on_pay_period_id", using: :btree
+    t.integer "approved_status"
+    t.integer "approving_admin_profile_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "pay_period_id"
+    t.index ["pay_period_id"], name: "index_timesheets_on_pay_period_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "username",                        null: false
-    t.string   "profileable_type"
-    t.integer  "profileable_id"
-    t.string   "crypted_password"
-    t.string   "salt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "remember_me_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "username", null: false
+    t.string "profileable_type"
+    t.integer "profileable_id"
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "remember_me_token"
     t.datetime "remember_me_token_expires_at"
-    t.string   "full_name"
-    t.integer  "active_status"
-    t.string   "reset_password_token"
+    t.string "full_name"
+    t.integer "active_status"
+    t.string "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
-    t.index ["profileable_type", "profileable_id"], name: "index_users_on_profileable_type_and_profileable_id", using: :btree
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
-    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
+    t.index ["profileable_type", "profileable_id"], name: "index_users_on_profileable_type_and_profileable_id"
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
